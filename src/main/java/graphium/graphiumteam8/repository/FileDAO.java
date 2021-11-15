670db93c21262ba3a4c44ff7b2394d46e1f1357a
Sean Marney <marneys@cardiff.ac.uk>
2021-11-15 13:55:53 +0000

    Created DAO (data access object) for file which provides the CRUD repository

--- src/main/java/graphium/graphiumteam8/repository/FileDAO.java
+++ src/main/java/graphium/graphiumteam8/repository/FileDAO.java
@@ -3,6 +3,8 @@ package graphium.graphiumteam8.repository;
 import graphium.graphiumteam8.entity.File;
 import org.springframework.data.repository.CrudRepository;
 
+// File DAO (Data Access Object) provides CrudRepository, which has built-in methods for a CRUD application
+
 public interface FileDAO extends CrudRepository<File, Long> {
-    
+
 }
