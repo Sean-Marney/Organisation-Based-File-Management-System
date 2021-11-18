af8e15d1e42f53a14ab491d23f1436c679790f95
c1989961 <InegbedionOI@cardiff.ac.uk>
2021-11-18 16:35:29 +0000

    added a controller for the registration page

--- src/main/java/graphium/graphiumteam8/controller/RegistrationController.java
+++ src/main/java/graphium/graphiumteam8/controller/RegistrationController.java
@@ -1,20 +1,20 @@
-//package graphium.graphiumteam8.controller;
-//
-//import org.springframework.stereotype.Controller;
-//import org.springframework.ui.Model;
-//import org.springframework.web.bind.annotation.GetMapping;
-//
-//@Controller
-//public class RegistrationController {
-//
-////    @GetMapping({"/","registration"})
-////    public String registration(Model model) {
-////        return "registration";
-////    }
-//
-//    @GetMapping({"/registration"})
-//    public String getRegistration(Model model) {
+package graphium.graphiumteam8.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class RegistrationController {
+
+//    @GetMapping({"/","registration"})
+//    public String registration(Model model) {
 //        return "registration";
 //    }
-//
-//}
+
+    @GetMapping({"/registration"})
+    public String getRegistration(Model model) {
+        return "registration";
+    }
+
+}
