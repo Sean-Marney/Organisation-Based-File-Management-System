ce8353ba5299d187f5336caa8137e95e81cdc072
c1989961 <InegbedionOI@cardiff.ac.uk>
2021-11-18 16:34:14 +0000

    created a new page for login

--- src/test/java/graphium/graphiumteam8/GraphiumTeam8ApplicationTests.java
+++ src/test/java/graphium/graphiumteam8/GraphiumTeam8ApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class GraphiumTeam8ApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+//    @Test
+//    void contextLoads() {
+//    }
 
 }
