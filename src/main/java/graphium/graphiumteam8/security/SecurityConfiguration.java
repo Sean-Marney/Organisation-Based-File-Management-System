1e0e7895607393245c8539884bdb65c223e04a9a
c1989961 <InegbedionOI@cardiff.ac.uk>
2021-11-20 20:11:26 +0000

    editted the login html and controller

--- src/main/java/graphium/graphiumteam8/security/SecurityConfiguration.java
+++ src/main/java/graphium/graphiumteam8/security/SecurityConfiguration.java
@@ -2,14 +2,44 @@ package graphium.graphiumteam8.security;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 
 @Configuration
 @Order
 public class SecurityConfiguration  {
 
+
+
     @Autowired
-    
+    UserDetailsService userDetailsService;
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService);
+    }
+
+    @Bean
+    public PasswordEncoder getPasswordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/coach").hasRole("COACH")
+                .and().formLogin();
+    }
+
+
+
+
 }
