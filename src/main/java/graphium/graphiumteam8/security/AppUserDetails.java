1ad0a92145ffd59add2d6187fece63071be6bc30
c1989961 <InegbedionOI@cardiff.ac.uk>
2021-11-29 06:33:41 +0000

    worked on the spring security login criteria

--- src/main/java/graphium/graphiumteam8/security/AppUserDetails.java
+++ src/main/java/graphium/graphiumteam8/security/AppUserDetails.java
@@ -1,59 +1,73 @@
-//package graphium.graphiumteam8.security;
-//
-//import org.springframework.security.core.GrantedAuthority;
-//import org.springframework.security.core.authority.SimpleGrantedAuthority;
-//import org.springframework.security.core.userdetails.UserDetails;
-//
-//import java.util.Arrays;
-//import java.util.Collection;
-//import java.util.List;
-//
-//public class AppUserDetails implements UserDetails {
-//    private String username;
-//    private String password;
-//    private List<GrantedAuthority> authorities;
-//    private Boolean enabled;
-//
-//    public AppUserDetails(AppUser user) {
-//        this.username = user.getUsername();
+package graphium.graphiumteam8.security;
+
+import graphium.graphiumteam8.entity.Role;
+import graphium.graphiumteam8.entity.User;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.*;
+
+public class AppUserDetails implements UserDetails {
+    private String username;
+    private String password;
+    private List<GrantedAuthority> authorities;
+    private Boolean enabled;
+
+    public AppUserDetails(User user) {
+        this.username = user.getUsername();
 //        this.password = user.getPassword();
-//        this.enabled = user.getEnabled();
-//        this.authorities = Arrays.asList(new SimpleGrantedAuthority(user.getRole()));
-//    }
-//
-//    @Override
-//    public String getPassword() {
+        this.enabled = user.getEnabled();
+        this.authorities = Arrays.asList(new SimpleGrantedAuthority(user.getRole()));
+//        this.authorities = user.getRoles();
+    }
+
+    @Override
+    public String getPassword() {
 //        return password;
-//    }
-//
-//    @Override
-//    public String getUsername() {
-//        return username;
-//    }
-//
-//    @Override
-//    public boolean isAccountNonExpired() {
-//        return enabled;
-//    }
-//
+        return "pass";
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return enabled;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return authorities;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return enabled;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return enabled;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return enabled;
+    }
+
 //    @Override
 //    public Collection<? extends GrantedAuthority> getAuthorities() {
+//        Set<Role> roles=authorities;
+//        List<SimpleGrantedAuthority> authorities = new ArrayList<>();
+//
+//        for(Role role:roles){
+//            authorities.add(new SimpleGrantedAuthority(role.getName()));
+//        }
+//
 //        return authorities;
 //    }
-//
-//    @Override
-//    public boolean isAccountNonLocked() {
-//        return enabled;
-//    }
-//
-//    @Override
-//    public boolean isCredentialsNonExpired() {
-//        return enabled;
-//    }
-//
-//    @Override
-//    public boolean isEnabled() {
-//        return enabled;
-//    }
-//
-//}
+
+}
