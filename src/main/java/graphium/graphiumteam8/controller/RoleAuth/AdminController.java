50843b7c816f59339db64bfb166df9b8cb83412d
c2040142 <ahmedms4@cardiff.ac.uk>
2021-12-07 04:10:03 +0000

    some validation also working on tests

--- src/main/java/graphium/graphiumteam8/controller/RoleAuth/AdminController.java
+++ src/main/java/graphium/graphiumteam8/controller/RoleAuth/AdminController.java
@@ -1,6 +1,7 @@
 package graphium.graphiumteam8.controller.RoleAuth;
 
-import graphium.graphiumteam8.entity.User;
+//import graphium.graphiumteam8.entity.User;
+import org.apache.catalina.User;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
