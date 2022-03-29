ba25f3d6fd9d631b198994def08834a2c271e3b4
Sean Marney <marneys@cardiff.ac.uk>
2022-03-29 20:15:31 +0100

    added sample test

---
+++ src/test/java/graphium/graphiumteam8/SampleJUnitTest.java
@@ -0,0 +1,13 @@
+package graphium.graphiumteam8;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class SampleJUnitTest {
+
+    @Test
+    void test() {
+        System.out.println("Test successful");
+    }
+}
--- src/test/java/graphium/graphiumteam8/controller/FileControllerTests.java
+++ src/test/java/graphium/graphiumteam8/controller/FileControllerTests.java
@@ -1,52 +1,52 @@
-package graphium.graphiumteam8.controller;
-
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.mock.web.MockMultipartFile;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
-
-@SpringBootTest
-@AutoConfigureMockMvc
-public class FileControllerTests {
-
-    @Autowired
-    MockMvc mockMvc;
-
-//    @Test
-//    @DisplayName("File upload test")
-//    void shouldUploadAFile() throws Exception {
+//package graphium.graphiumteam8.controller;
 //
-//        // Creating a mock file to test
-//        MockMultipartFile mockMultipartFile = new MockMultipartFile(
-//                "file",
-//                "test-file.txt",
-//                "text/plain",
-//                "Content"
-//                        .getBytes()
-//        );
-//        // MockMvc passes the mock file through the /upload endpoint
-//        this.mockMvc.perform(MockMvcRequestBuilders.multipart("/upload")
-//                        .file(mockMultipartFile))
-//                .andExpect(MockMvcResultMatchers.status().isOk()); // Checks status is OK (HTTP response = 200)
+//import org.junit.jupiter.api.DisplayName;
+//import org.junit.jupiter.api.Test;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.mock.web.MockMultipartFile;
+//import org.springframework.test.web.servlet.MockMvc;
+//import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+//import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 //
-//    }
-
-//    @Test
-//    @DisplayName("File download test")
-//    void shouldDownloadAFile() throws Exception {
+//@SpringBootTest
+//@AutoConfigureMockMvc
+//public class FileControllerTests {
 //
-//        MockMultipartFile mockMultipartFile = new MockMultipartFile(
-//                "file",
-//                "test-file.txt",
-//                "text/plain",
-//                "Content"
-//                        .getBytes()
-//        );
-//        this.mockMvc.perform(MockMvcRequestBuilders.multipart("/download"));
-//    }
-}
+//    @Autowired
+//    MockMvc mockMvc;
+//
+////    @Test
+////    @DisplayName("File upload test")
+////    void shouldUploadAFile() throws Exception {
+////
+////        // Creating a mock file to test
+////        MockMultipartFile mockMultipartFile = new MockMultipartFile(
+////                "file",
+////                "test-file.txt",
+////                "text/plain",
+////                "Content"
+////                        .getBytes()
+////        );
+////        // MockMvc passes the mock file through the /upload endpoint
+////        this.mockMvc.perform(MockMvcRequestBuilders.multipart("/upload")
+////                        .file(mockMultipartFile))
+////                .andExpect(MockMvcResultMatchers.status().isOk()); // Checks status is OK (HTTP response = 200)
+////
+////    }
+//
+////    @Test
+////    @DisplayName("File download test")
+////    void shouldDownloadAFile() throws Exception {
+////
+////        MockMultipartFile mockMultipartFile = new MockMultipartFile(
+////                "file",
+////                "test-file.txt",
+////                "text/plain",
+////                "Content"
+////                        .getBytes()
+////        );
+////        this.mockMvc.perform(MockMvcRequestBuilders.multipart("/download"));
+////    }
+//}
