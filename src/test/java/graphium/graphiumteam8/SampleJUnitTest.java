d7f8b9075711eb556084f83e9b8bdfb7134a54a6
Sean Marney <marneys@cardiff.ac.uk>
2022-03-30 11:39:13 +0100

    removed @SpringBootTest

--- src/test/java/graphium/graphiumteam8/SampleJUnitTest.java
+++ src/test/java/graphium/graphiumteam8/SampleJUnitTest.java
@@ -3,7 +3,6 @@ package graphium.graphiumteam8;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 public class SampleJUnitTest {
 
     // other junit tests temporarily deleted for debugging error
